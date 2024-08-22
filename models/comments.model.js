e66eb09d1e6e549c21cef50f370ace14496304e4
Shama Khan <shama1309khan@gmail.com>
2024-08-22 18:49:53 +0530

    typo fixed

--- models/comments.model.js
+++ models/comments.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const commmentSchema = new mongoose.Schema({
     text : {type:String, required : true},
     author : {type : mongoose.Schema.Types.ObjectId, ref : 'User', required : true},
-    post : {type : mongoose.Schema.Types.ObjectId, ref : 'Post', requireD : true}
+    post : {type : mongoose.Schema.Types.ObjectId, ref : 'Post', required : true}
 })
 
 export default Comments = mongoose.model('Comments', commmentSchema);
